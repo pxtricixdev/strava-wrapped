6b2469dbad64e33bc8b1c6063704f0cf4b1e7243
Patricia <patricia.abadaranda@gmail.com>
2026-04-16 17:02:13 +0200

    feat: add dev dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        types: ["node"],
+      },
+    },
+  },
+  runtimeConfig: {
+    stravaClientSecret: process.env.STRAVA_CLIENT_SECRET,
+    public: {
+      stravaClientId: process.env.STRAVA_CLIENT_ID,
+    },
+  },
+});
